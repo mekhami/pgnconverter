ac2bb0f0f913586f2281f33b5d6223ecb8c8186d
Lawrence Vanderpool <lawrence.vanderpool@gmail.com>
2016-04-12 13:43:51 -0500

    header functionality

--- converter.py
+++ converter.py
@@ -1,7 +1,17 @@
 class Game(object):
     def __init__(self, pgn):
         self.pgn = pgn
+        self.headers = {}
+
+        self.set_headers()
 
     def move_list(self):
         with open(self.pgn) as f:
             return f.readlines()[8]
+
+    def set_headers(self):
+        with open(self.pgn) as f:
+            for line in f.readlines()[0:7]:
+                trimmed_line = line[1:-1]  # Get rid of brackets
+                param, value = trimmed_line.split(" ")
+                self.headers[param] = value
--- tests.py
+++ tests.py
@@ -10,3 +10,7 @@ class PGNConverterTest(unittest.TestCase):
     
     def test_get_move_list(self):
         self.assertIn("1. e4 c5", self.game.move_list())
+
+    def test_get_headers(self):
+        self.assertIn('Event', self.game.headers)
+        self.assertIn('Black', self.game.headers)
